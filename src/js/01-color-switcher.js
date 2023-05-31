05417532f7f10f9ce78b1e73057bcf9282a7a3ec
Eugeshakw <eugeshakw@gmail.com>
2023-05-31 19:26:40 +0300

    update-hw-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const refs = {
+btnStart: document.querySelector('[data-start]'),
+btnStop: document.querySelector('[data-stop]'),
+
+};
+  
+let colorRandomTimer = null;
+
+refs.btnStart.addEventListener('click', (evt) =>{
+    evt.preventDefault();
+    colorRandomTimer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.btnStart.disabled = true; 
+    refs.btnStop.disabled = false;
+
+
+})
+
+refs.btnStop.addEventListener('click', (evt) =>{
+    evt.preventDefault();
+    clearInterval(colorRandomTimer);
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+
+})
+  

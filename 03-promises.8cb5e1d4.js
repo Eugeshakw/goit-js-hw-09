119e669f1a32fcb507c64121bc208c6f3dc76f0e
Eugeshakw <120225890+Eugeshakw@users.noreply.github.com>
2023-06-01 20:24:46 +0000

    Deploying to gh-pages from @ Eugeshakw/goit-js-hw-09@e719d62ca6312e92c7e14e84996f5f5fd1e1678c 🚀

---
+++ 03-promises.8cb5e1d4.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".form"),btn:document.querySelector("button")},t=e.form.elements.delay,n=e.form.elements.step;function o(e,t){return new Promise((function(e,t){setTimeout((function(){po}))}))}e.form.elements.amount.addEventListener("submit",o),n.addEventListener("submit",o),t.addEventListener("click",o)}();
+//# sourceMappingURL=03-promises.8cb5e1d4.js.map

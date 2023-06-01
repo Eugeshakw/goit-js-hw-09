e719d62ca6312e92c7e14e84996f5f5fd1e1678c
Eugeshakw <eugeshakw@gmail.com>
2023-06-01 23:24:21 +0300

    update-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,27 @@
+
+
+const refs = {
+  form: document.querySelector('.form'),
+  btn: document.querySelector('button'),
+  
+}
+
+const delay = refs.form.elements.delay;
+const step = refs.form.elements.step;
+const amount = refs.form.elements.amount;
+amount.addEventListener('submit', createPromise)
+step.addEventListener('submit', createPromise)
+delay.addEventListener('click', createPromise)
+
+
+
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve = po
+    })
+  })
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
@@ -6,3 +29,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+

dcba8466577ae2cdcdec206dcc03db0ad7c53464
Eugeshakw <120225890+Eugeshakw@users.noreply.github.com>
2023-06-01 21:50:22 +0000

    Deploying to gh-pages from @ Eugeshakw/goit-js-hw-09@00fb2253c4882f5479a61a5cf652b49d2d988098 🚀

--- 03-promises.d936f7e7.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".form"),btn:document.querySelector("button")},t=e.form.elements.delay,n=e.form.elements.step;function o(e,t){return new Promise(((e,t)=>{setTimeout((()=>{po}))}))}e.form.elements.amount.addEventListener("submit",o),n.addEventListener("submit",o),t.addEventListener("click",o);
-//# sourceMappingURL=03-promises.d936f7e7.js.map
